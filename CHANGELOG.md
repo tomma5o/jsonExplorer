76aa223165b5e01e766090e12e55a8195ced72e2
Tommaso Poletti <tommaso.poletti@spindox.it>
2018-12-02 02:27:11 +0100

    fixed small bugs in main function & in CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,14 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-## [2.5.0] - 2018-08-11
+## [0.0.1] - 2018-12-02
 ### Added
 - main function that manage and filter the entry object
 
 <!-- ### Changed -->
 
-<!-- ### Fixed -->
+### Fixed
+- Small bugs in main function
 
 ## TODO
 - Add logic for manage more than 2 level of nesting
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { errorMsg, usageMsg } = require('./utils/logs');
 const {stdin, stdout, stderr, argv} = process
 
 // npm
-import is from '@sindresorhus/is';
+const is = require('@sindresorhus/is');
 
 class JsonExplorer {
     constructor() {
@@ -10,29 +10,6 @@ class JsonExplorer {
         this.filter = argv[2];
         this.usage = usageMsg;
     }
-
-    // extractString(data, nextValue) {
-    //     return data
-    // }
-
-    // extractNumber(data, nextValue) {
-    //     return data
-    // }
-
-    // extractArray(data, nextValue) {
-    //     if (nextValue) {
-    //         return data.reduce((acc, e) => [...acc, {[nextValue]: e[nextValue]}],[]);
-    //     }
-    //     return data
-    // }
-
-    // extractObject(data, nextValue) {
-    //     if (nextValue) {
-    //         main(data, this.filter)
-    //         return;
-    //     }
-    //     return data
-    // }
 
     main(data, filter = [], skip = false) {
         const currentKey = skip ? filter[1] : filter.shift();
@@ -44,15 +21,15 @@ class JsonExplorer {
                 console.log("is.array")
                 if (nextValue) {
                     return currentValue.reduce((acc, e) => {
-                        return [...acc, main(e[nextValue], filter, true)]
+                        return [...acc, this.main(e[nextValue], filter, true)];
                     }, []);
                 }
-                return currentValue
+                return currentValue;
                 break;
             case is.object(currentValue):
                 console.log("is.object")
                 if (nextValue) {
-                    return main(currentValue[nextValue], filter, true);
+                    return this.main(currentValue[nextValue], filter, true);
                 }
                 return currentValue;
                 break;
@@ -66,13 +43,17 @@ class JsonExplorer {
         stdin.setEncoding('utf8')
 
         if (!this.filter || stdin.isTTY) {
-            stderr.write(this.usage)
-            process.exit(2)
+            stderr.write(this.usage);
+            process.exit(2);
         }
         
         stdin.on('readable', () => {
             this.input = stdin.read();
-            this.input && this.main(this.input.split('.'), this.filter)
+            if (this.input) {
+                var parsedData = JSON.parse(this.input);
+                const objFiltered = this.main(parsedData, this.filter.split('.'));
+                console.log(objFiltered)
+            }
         })
     }
 }
